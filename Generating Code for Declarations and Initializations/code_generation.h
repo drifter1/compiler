6e52080e4ddd47388fbd2136ab79cb5a09306cf1
drifter1 <drifterkosmas@gmail.com>
2025-09-26 19:25:11 +0300

    Relates to #5: remove the per article/step folders

--- Generating Code for Declarations and Initializations/code_generation.h
+++
@@ -1,6 +0,0 @@
-
-/* main assembly code generation function */
-void generate_code();
-
-/* generation declaration assembly code */
-void generate_data_declarations(FILE *fp);
--- Simple C Language/example1.c
+++
@@ -1,6 +0,0 @@
-int i;
-double val = 2.5;
-for(i = 0; i < 10; i++){
-	val = val*i + i;
-}
-print(val);
