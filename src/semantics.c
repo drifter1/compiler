7e0de11db16cf9147e1314ac8c99b4229d275370
drifter1 <drifterkosmas@gmail.com>
2026-03-24 13:16:54 +0200

    first steps of the new now separated semantic analysis (related to #6 and #11)

--- src/semantics.c
+++ src/semantics.c
@@ -1,384 +1,103 @@
 #include "../include/semantics.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-// line number
-extern int yylineno;
+void semantic_analysis(ast_node *ast_node) {
 
-/* flag variable that shows revisit in assignment expression */
-int cont_revisit = 0; // 1: contains revisit, 0: not
-
-// actual types are: INT_TYPE, REAL_TYPE, CHAR_TYPE
-
-int get_result_type(int type_1, int type_2,
-                    int op_type) { /* type check and result type */
-    switch (op_type) {
-    case NONE: /* type compatibility only, '1': compatible */
-        // first type INT
-        if (type_1 == INT_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return 1;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type REAL
-        else if (type_1 == REAL_TYPE) {
-            // second type INT, REAL or CHAR
-            if (type_2 == INT_TYPE || type_2 == REAL_TYPE ||
-                type_2 == CHAR_TYPE) {
-                return 1;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return 1;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
+    switch (ast_node->kind) {
+    case PROGRAM:
+        printf("Semantic analysis of Program Node\n");
+        semantic_analysis_list(ast_node->as.program.declarations);
+        semantic_analysis_list(ast_node->as.program.functions);
+        semantic_analysis(ast_node->as.program.main_function);
+        break;
+    case DECLARATION:
+        printf("Semantic analysis of Declaration Node\n");
+        list_node *head = ast_node->as.declaration.names;
+        while (head != NULL) {
+            symtab_entry *entry = (symtab_entry *)head->data;
+            entry->as.variable.d_type = ast_node->as.declaration.d_type;
+            head = head->next;
         }
         break;
-    /* ---------------------------------------------------------- */
-    case ARITHM_OP: /* arithmetic operator */
-        // first type INT
-        if (type_1 == INT_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            }
-            // second type REAL
-            else if (type_2 == REAL_TYPE) {
-                return REAL_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type REAL
-        else if (type_1 == REAL_TYPE) {
-            // second type INT, REAL or CHAR
-            if (type_2 == INT_TYPE || type_2 == REAL_TYPE ||
-                type_2 == CHAR_TYPE) {
-                return REAL_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return CHAR_TYPE;
-            }
-            // second type REAL
-            else if (type_2 == REAL_TYPE) {
-                return REAL_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case CONSTANT:
+        printf("Semantic analysis of Constant Node\n");
         break;
-    /* ---------------------------------------------------------- */
-    case INCR_OP: /* special case of INCR */
-        // type INT
-        if (type_1 == INT_TYPE) {
-            return INT_TYPE;
-        }
-        // type REAL
-        else if (type_1 == REAL_TYPE) {
-            return REAL_TYPE;
-        }
-        // type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            return CHAR_TYPE;
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case FUNCTION:
+        printf("Semantic analysis of Function Node\n");
+        semantic_analysis(ast_node->as.function.function_tail);
         break;
-    /* ---------------------------------------------------------- */
-    case BOOL_OP: /* Boolean operator */
-        // first type INT
-        if (type_1 == INT_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return CHAR_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case FUNCTION_TAIL:
+        printf("Semantic analysis of Function Tail Node\n");
+        semantic_analysis_list(ast_node->as.function_tail.declarations);
+        semantic_analysis_list(ast_node->as.function_tail.statements);
         break;
-    /* ---------------------------------------------------------- */
-    case NOT_OP: /* special case of NOTOP */
-        // type INT
-        if (type_1 == INT_TYPE) {
-            return INT_TYPE;
-        }
-        // type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            return INT_TYPE;
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case IF_STATEMENT:
+        printf("Semantic analysis of If Statement Node\n");
+        semantic_analysis(ast_node->as.if_statement.condition);
+        semantic_analysis_list(ast_node->as.if_statement.if_branch);
+        semantic_analysis_list(ast_node->as.if_statement.else_if_branches);
+        semantic_analysis_list(ast_node->as.if_statement.else_branch);
         break;
-    /* ---------------------------------------------------------- */
-    case REL_OP: /* Relational operator */
-        // first type INT
-        if (type_1 == INT_TYPE) {
-            // second type INT, REAL or CHAR
-            if (type_2 == INT_TYPE || type_2 == REAL_TYPE ||
-                type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else if (type_1 == REAL_TYPE) {
-            // second type INT, REAL or CHAR
-            if (type_2 == INT_TYPE || type_2 == REAL_TYPE ||
-                type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            // second type INT, REAL or CHAR
-            if (type_2 == INT_TYPE || type_2 == REAL_TYPE ||
-                type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case EXPRESSION_BINARY:
+        printf("Semantic analysis of Binary Expression Node\n");
         break;
-    /* ---------------------------------------------------------- */
-    case EQU_OP: /* Equality operator */
-        // first type INT
-        if (type_1 == INT_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else if (type_1 == REAL_TYPE) {
-            // second type REAL
-            if (type_2 == REAL_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        }
-        // first type CHAR
-        else if (type_1 == CHAR_TYPE) {
-            // second type INT or CHAR
-            if (type_2 == INT_TYPE || type_2 == CHAR_TYPE) {
-                return INT_TYPE;
-            } else {
-                type_error(type_1, type_2, op_type);
-            }
-        } else {
-            type_error(type_1, type_2, op_type);
-        }
+    case EXPRESSION_UNARY:
+        printf("Semantic analysis of Unary Expression Node\n");
         break;
-    /* ---------------------------------------------------------- */
-    default: /* wrong choice case */
-        fprintf(stderr, "Error in operator selection!\n");
-        exit(EXIT_FAILURE);
+    case VARIABLE_REFERENCE:
+        printf("Semantic analysis of Variable Reference Node\n");
+        break;
+    case FUNCTION_CALL:
+        printf("Semantic analysis of Function Call Node\n");
+        break;
+    case ELSE_IF:
+        printf("Semantic analysis of Else If Node\n");
+        semantic_analysis(ast_node->as.else_if.condition);
+        semantic_analysis_list(ast_node->as.else_if.else_if_branch);
+        break;
+    case FOR_LOOP:
+        printf("Semantic analysis of For Loop Node\n");
+        semantic_analysis(ast_node->as.for_loop.initialize);
+        semantic_analysis(ast_node->as.for_loop.condition);
+        semantic_analysis(ast_node->as.for_loop.increment);
+        semantic_analysis_list(ast_node->as.for_loop.for_branch);
+        break;
+    case ASSIGNMENT:
+        printf("Semantic analysis of Assignment Node\n");
+        break;
+    case WHILE_LOOP:
+        printf("Semantic analysis of While Loop Node\n");
+        semantic_analysis(ast_node->as.while_loop.condition);
+        semantic_analysis_list(ast_node->as.while_loop.while_branch);
+        break;
+    case JUMP_STATEMENT:
+        printf("Semantic analysis of Jump Statement Node\n");
+        break;
+    case PRINT_STATEMENT:
+        printf("Semantic analysis of Print Statement Node\n");
+        break;
+    case INPUT_STATEMENT:
+        printf("Semantic analysis of Input Statement Node\n");
+        break;
+    case RETURN_STATEMENT:
+        printf("Semantic analysis of Return Statement Node\n");
+        break;
     }
 }
 
-void type_error(int type_1, int type_2, int op_type) { /* print type error */
-    fprintf(stderr, "Type conflict between ");
-    /* first type */
-    if (type_1 == INT_TYPE)
-        fprintf(stderr, "%s", "int");
-    else if (type_1 == REAL_TYPE)
-        fprintf(stderr, "%s", "real");
-    else if (type_1 == CHAR_TYPE)
-        fprintf(stderr, "%s", "char");
-    else
-        fprintf(stderr, "%s", "other");
-
-    fprintf(stderr, " and ");
-
-    /* second type */
-    if (type_2 == INT_TYPE)
-        fprintf(stderr, "%s", "int");
-    else if (type_2 == REAL_TYPE)
-        fprintf(stderr, "%s", "real");
-    else if (type_2 == CHAR_TYPE)
-        fprintf(stderr, "%s", "char");
-    else
-        fprintf(stderr, "%s", "other");
-
-    /* operator */
-    fprintf(stderr, " using op type ");
-    switch (op_type) {
-    case NONE:
-        fprintf(stderr, "%s", "NONE");
-        break;
-    case ARITHM_OP:
-        fprintf(stderr, "%s", "ARITHM_OP");
-        break;
-    case INCR_OP:
-        fprintf(stderr, "%s", "INCR_OP");
-        break;
-    case BOOL_OP:
-        fprintf(stderr, "%s", "BOOL_OP");
-        break;
-    case NOT_OP:
-        fprintf(stderr, "%s", "NOT_OP");
-        break;
-    case REL_OP:
-        fprintf(stderr, "%s", "REL_OP");
-        break;
-    case EQU_OP:
-        fprintf(stderr, "%s", "EQU_OP");
-        break;
-    default:
-        fprintf(stderr, "Error in operator selection!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    /* line */
-    fprintf(stderr, " in line %d\n", yylineno);
-
-    exit(EXIT_FAILURE);
-}
-
-int expression_data_type(AST_Node *node) {
-    /* temp nodes */
-    AST_Node_Arithm *temp_arithm;
-    AST_Node_Incr *temp_incr;
-    AST_Node_Bool *temp_bool;
-    AST_Node_Rel *temp_rel;
-    AST_Node_Equ *temp_equ;
-    AST_Node_Ref *temp_ref;
-    AST_Node_Const *temp_const;
-    AST_Node_Func_Call *temp_func_call;
-
-    /* return type depends on the AST node type */
-    switch (node->type) {
-    case ARITHM_NODE: /* arithmetic expression */
-        temp_arithm = (AST_Node_Arithm *)node;
-
-        /* set datatype again */
-        temp_arithm->data_type = get_result_type(
-            expression_data_type(
-                temp_arithm->left), /* data type of left expression */
-            expression_data_type(
-                temp_arithm->right), /* data type of right expression */
-            ARITHM_OP                /* operation type */
-        );
-
-        return temp_arithm->data_type;
-        break;
-    case INCR_NODE: /* special case of increment */
-        temp_incr = (AST_Node_Incr *)node;
-        return temp_incr->entry->st_type;
-        break;
-    case BOOL_NODE: /* boolean expression */
-        temp_bool = (AST_Node_Bool *)node;
-
-        /* set datatype again */
-        if (temp_bool->op != NOT) { /* AND or OR */
-            temp_bool->data_type = get_result_type(
-                expression_data_type(
-                    temp_bool->left), /* data type of left expression */
-                expression_data_type(
-                    temp_bool->right), /* data type of right expression */
-                BOOL_OP                /* operation type */
-            );
-        } else { /* NOT */
-            temp_bool->data_type = get_result_type(
-                expression_data_type(
-                    temp_bool->left), /* data type of left expression */
-                UNDEF,                /* there is no right expression */
-                NOT_OP                /* operation type */
-            );
-        }
-
-        return temp_bool->data_type;
-        break;
-    case REL_NODE: /* relational expression */
-        temp_rel = (AST_Node_Rel *)node;
-
-        /* set datatype again */
-        temp_rel->data_type = get_result_type(
-            expression_data_type(
-                temp_rel->left), /* data type of left expression  */
-            expression_data_type(
-                temp_rel->right), /* data type of right expression */
-            REL_OP                /* operation type */
-        );
-
-        return temp_rel->data_type;
-        break;
-    case EQU_NODE: /* equality expression */
-        temp_equ = (AST_Node_Equ *)node;
-
-        /* set datatype again */
-        temp_equ->data_type = get_result_type(
-            expression_data_type(
-                temp_equ->left), /* data type of left expression  */
-            expression_data_type(
-                temp_equ->right), /* data type of right expression */
-            EQU_OP                /* operation type */
-        );
-
-        return temp_equ->data_type;
-        break;
-    case REF_NODE: /* identifier reference */
-        temp_ref = (AST_Node_Ref *)node;
-        /* if "simple" type */
-        int type = temp_ref->entry->st_type;
-        if (type == INT_TYPE || type == REAL_TYPE || type == CHAR_TYPE) {
-            return temp_ref->entry->st_type;
-        }
-        /* if array or pointer */
-        else {
-            return temp_ref->entry->inf_type;
-        }
-        break;
-    case CONST_NODE: /* constant */
-        temp_const = (AST_Node_Const *)node;
-        return temp_const->const_type; /* constant data type */
-        break;
-    case FUNC_CALL: /* function call */
-        temp_func_call = (AST_Node_Func_Call *)node;
-
-        /* check if it needs revisit */
-        if (temp_func_call->entry->st_type == UNDEF) {
-            if (temp_func_call->entry->inf_type == UNDEF) {
-                cont_revisit = 1; /* contains revisit */
-                return INT_TYPE;  /*   dummy return   */
-            }
-        }
-
-        return temp_func_call->entry->inf_type; /* return type */
-        break;
-    default: /* wrong choice case */
-        fprintf(stderr, "Error in AST node selection!\n");
-        exit(EXIT_FAILURE);
+void semantic_analysis_list(list_node *list_head) {
+    list_node *head;
+    ast_node *ast_node;
+    head = list_head;
+    while (head != NULL) {
+        ast_node = (struct ast_node *)head->data;
+        semantic_analysis(ast_node);
+        head = head->next;
     }
 }
+
+data_type get_result_type(data_type type_1, data_type type_2,
+                          operator_type op_type) {}
+
+void type_error(data_type type_1, data_type type_2, operator_type op_type) {}
+
+data_type expression_data_type(ast_node *ast_node) {}
