86ec5c09a618f542e1f6a85ffd86a41b3b7189b0
drifter1 <drifterkosmas@gmail.com>
2026-04-06 13:04:03 +0300

    Introduction of three address code source and header files. First implementation (related to #14)

---
+++ include/tac.h
@@ -0,0 +1,135 @@
+#ifndef TAC_H
+#define TAC_H
+
+#include "ast.h"
+
+/* ----------------------Operator Codes--------------------- */
+
+typedef enum {
+    OP_NOP,
+    /* Assignment */
+    OP_ASSIGN,
+    /* Arithmetic */
+    OP_ADD,
+    OP_SUB,
+    OP_MUL,
+    OP_DIV,
+    OP_REM,
+    /* Unary */
+    OP_INC,
+    OP_DEC,
+    OP_NEG,
+    /* Logical */
+    OP_OR,
+    OP_AND,
+    OP_NOT,
+    /* Comparison */
+    OP_EQ,
+    OP_NEQ,
+    OP_GT,
+    OP_LT,
+    OP_GE,
+    OP_LE,
+    /* Control Flow */
+    OP_JUMP,
+    OP_JUMPIF,
+    OP_JUMPIFZ,
+    OP_LABEL,
+    /* I/O */
+    OP_PRINT,
+    OP_INPUT,
+    /* Functions */
+    OP_ARG,
+    OP_CALL,
+    OP_GETRET,
+    OP_PARAM,
+    OP_RET,
+    OP_RETV,
+    /* Type Casting */
+    OP_ICAST,
+    OP_FCAST,
+    OP_DCAST
+} op_code;
+
+/* -----------------------Operand Kinds--------------------- */
+
+typedef enum {
+    OPERAND_NONE,
+    OPERAND_CONST,
+    OPERAND_VAR,
+    OPERAND_TEMP,
+    OPERAND_LABEL,
+    OPERAND_STRING
+} operand_kind;
+
+/* -------------------------Operand------------------------- */
+
+typedef struct operand {
+    operand_kind kind;
+    union {
+        struct {
+            ast_node *constant_node;
+        } constant;
+        struct {
+            symtab_entry *entry;
+        } variable;
+        struct {
+            symtab_entry *entry;
+        } temporary;
+        struct {
+            const char *s;
+        } label;
+        struct {
+            const char *s;
+        } string;
+    } as;
+} operand;
+
+/* -----------------Three Address Code (TAC)---------------- */
+
+typedef struct tac {
+    op_code op;
+    operand result;
+    operand arg1;
+    operand arg2;
+} tac;
+
+/* -------------------------TAC List------------------------ */
+
+typedef struct {
+    tac *t;
+    int size;
+    int capacity;
+} tac_list;
+
+/* ------------------TAC LIST CONFIGURATION----------------- */
+
+#define TAC_LIST_INIT_CAP 8
+
+/* --------------------TAC LIST STRUCTURE------------------- */
+
+extern tac_list *tl;
+
+/* ----------------------MAIN FUNCTIONS--------------------- */
+
+tac tac_create(op_code op, operand result, operand arg1, operand arg2);
+void tac_print(tac t);
+
+void tac_list_init();
+void tac_list_add(tac t);
+void tac_list_free();
+void tac_list_print();
+
+/* ---------------------HELPER FUNCTIONS-------------------- */
+
+operand op_none();
+operand op_const(ast_node *constant_node);
+operand op_var(symtab_entry *entry);
+operand op_temp(symtab_entry *entry);
+operand op_label(const char *s);
+operand op_string(const char *s);
+
+const char *op_code_to_string(op_code op);
+const char *operand_to_string(operand arg);
+
+#endif /* TAC_H */
---
+++ src/tac.c
@@ -0,0 +1,284 @@
+#include "../include/tac.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/* --------------------TAC LIST STRUCTURE------------------- */
+
+tac_list *tl;
+
+/* ----------------------MAIN FUNCTIONS--------------------- */
+
+tac tac_create(op_code op, operand result, operand arg1, operand arg2) {
+    tac t = {op, result, arg1, arg2};
+    return t;
+}
+
+void tac_print(tac t) {
+    const char *op_str = op_code_to_string(t.op);
+    const char *result_str = operand_to_string(t.result);
+    const char *arg1_str = operand_to_string(t.arg1);
+    const char *arg2_str = operand_to_string(t.arg2);
+
+    switch (t.op) {
+    case OP_NOP:
+        printf("%s\n", op_str);
+        break;
+    case OP_ASSIGN:
+        printf("%s %s %s\n", result_str, op_str, arg1_str);
+        break;
+    case OP_ADD:
+    case OP_SUB:
+    case OP_MUL:
+    case OP_DIV:
+    case OP_REM:
+        printf("%s = %s %s %s\n", result_str, arg1_str, op_str, arg2_str);
+        break;
+    case OP_INC:
+    case OP_DEC:
+        printf("%s%s\n", result_str, op_str);
+        break;
+    case OP_NEG:
+        printf("%s = %s%s\n", result_str, op_str, arg1_str);
+        break;
+    case OP_OR:
+    case OP_AND:
+        printf("%s = %s %s %s\n", result_str, arg1_str, op_str, arg2_str);
+        break;
+    case OP_NOT:
+        printf("%s = %s %s\n", result_str, op_str, arg1_str);
+        break;
+    case OP_EQ:
+    case OP_NEQ:
+    case OP_GT:
+    case OP_LT:
+    case OP_GE:
+    case OP_LE:
+        printf("%s = %s %s %s\n", result_str, arg1_str, op_str, arg2_str);
+        break;
+    case OP_JUMP:
+        printf("%s %s\n", op_str, result_str);
+        break;
+    case OP_JUMPIF:
+    case OP_JUMPIFZ:
+        printf("%s %s, %s\n", op_str, result_str, arg1_str);
+        break;
+    case OP_LABEL:
+        printf("%s%s\n", result_str, op_str);
+        break;
+    case OP_PRINT:
+    case OP_INPUT:
+    case OP_ARG:
+    case OP_CALL:
+    case OP_GETRET:
+    case OP_PARAM:
+        printf("%s %s\n", op_str, result_str);
+        break;
+    case OP_RET:
+        printf("%s\n", op_str);
+        break;
+    case OP_RETV:
+        printf("%s %s\n", op_str, result_str);
+        break;
+    case OP_ICAST:
+    case OP_FCAST:
+    case OP_DCAST:
+        printf("%s = %s(%s)\n", op_str, result_str, arg1_str);
+    }
+}
+
+void tac_list_init() {
+    tl = (tac_list *)malloc(sizeof(tac_list));
+    tl->t = (tac *)malloc(TAC_LIST_INIT_CAP * sizeof(tac));
+    tl->size = 0;
+    tl->capacity = TAC_LIST_INIT_CAP;
+}
+
+void tac_list_add(tac t) {
+    if (tl->size >= tl->capacity) {
+        tl->capacity *= 2;
+        tl->t = (tac *)realloc(tl->t, tl->capacity * sizeof(tac));
+    }
+    tl->t[tl->size++] = t;
+}
+
+void tac_list_free() {
+    free(tl->t);
+    free(tl);
+}
+
+void tac_list_print() {
+    int i;
+    for (i = 0; i < tl->size; i++) {
+        tac_print(tl->t[i]);
+    }
+}
+
+/* ---------------------HELPER FUNCTIONS-------------------- */
+
+operand op_none() {
+    operand operand;
+    operand.kind = OPERAND_NONE;
+    return operand;
+}
+
+operand op_const(ast_node *constant_node) {
+    operand operand;
+    operand.kind = OPERAND_CONST;
+    operand.as.constant.constant_node = constant_node;
+    return operand;
+}
+operand op_var(symtab_entry *entry) {
+    operand operand;
+    operand.kind = OPERAND_VAR;
+    operand.as.variable.entry = entry;
+    return operand;
+}
+
+operand op_temp(symtab_entry *entry) {
+    operand operand;
+    operand.kind = OPERAND_TEMP;
+    operand.as.temporary.entry = entry;
+    return operand;
+}
+
+operand op_label(const char *s) {
+    operand operand;
+    operand.kind = OPERAND_LABEL;
+    operand.as.label.s = s;
+    return operand;
+}
+
+operand op_string(const char *s) {
+    operand operand;
+    operand.kind = OPERAND_STRING;
+    operand.as.string.s = s;
+    return operand;
+}
+
+const char *op_code_to_string(op_code op) {
+    switch (op) {
+    case OP_NOP:
+        return ";";
+    /* Assignment */
+    case OP_ASSIGN:
+        return "=";
+    /* Arithmetic */
+    case OP_ADD:
+        return "+";
+    case OP_SUB:
+        return "-";
+    case OP_MUL:
+        return "*";
+    case OP_DIV:
+        return "/";
+    case OP_REM:
+        return "%%";
+    /* Unary */
+    case OP_INC:
+        return "++";
+    case OP_DEC:
+        return "--";
+    case OP_NEG:
+        return "-";
+    /* Logical */
+    case OP_OR:
+        return "or";
+    case OP_AND:
+        return "and";
+    case OP_NOT:
+        return "not";
+    /* Comparison */
+    case OP_EQ:
+        return "==";
+    case OP_NEQ:
+        return "!=";
+    case OP_GT:
+        return ">";
+    case OP_LT:
+        return "<";
+    case OP_GE:
+        return ">=";
+    case OP_LE:
+        return "<=";
+    /* Control Flow */
+    case OP_JUMP:
+        return "jump";
+    case OP_JUMPIF:
+        return "jumpif";
+    case OP_JUMPIFZ:
+        return "jumpifz";
+    case OP_LABEL:
+        return ":";
+    /* I/O */
+    case OP_PRINT:
+        return "print";
+    case OP_INPUT:
+        return "input";
+    /* Functions */
+    case OP_ARG:
+        return "arg";
+    case OP_CALL:
+        return "call";
+    case OP_GETRET:
+        return "getret";
+    case OP_PARAM:
+        return "param";
+    case OP_RET:
+        return "ret";
+    case OP_RETV:
+        return "retv";
+    /* Type Casting */
+    case OP_ICAST:
+        return "icast";
+    case OP_FCAST:
+        return "fcast";
+    case OP_DCAST:
+        return "dcast";
+    default:
+        return "_error";
+    }
+}
+
+const char *operand_to_string(operand arg) {
+    char *operand_string;
+    switch (arg.kind) {
+    case OPERAND_NONE:
+        return "_none";
+    case OPERAND_CONST:
+        operand_string = (char *)malloc(40 * sizeof(char));
+        switch (arg.as.constant.constant_node->as.constant.d_type) {
+        case UNDEF_TYPE:
+            sprintf(operand_string, "_error");
+            break;
+        case INT_TYPE:
+            sprintf(operand_string, "%d",
+                    arg.as.constant.constant_node->as.constant.val.ival);
+            break;
+        case CHAR_TYPE:
+            sprintf(operand_string, "%d",
+                    arg.as.constant.constant_node->as.constant.val.cval);
+            break;
+        case FLOAT_TYPE:
+            sprintf(operand_string, "%f",
+                    arg.as.constant.constant_node->as.constant.val.fval);
+            break;
+        case DOUBLE_TYPE:
+            sprintf(operand_string, "%lf",
+                    arg.as.constant.constant_node->as.constant.val.fval);
+            break;
+        case VOID_TYPE:
+            sprintf(operand_string, "_error");
+        }
+        return operand_string;
+    case OPERAND_VAR:
+        return arg.as.variable.entry->id;
+    case OPERAND_TEMP:
+        return arg.as.temporary.entry->id;
+    case OPERAND_LABEL:
+        return arg.as.label.s;
+    case OPERAND_STRING:
+        return arg.as.string.s;
+    default:
+        return "_error";
+    }
+}
