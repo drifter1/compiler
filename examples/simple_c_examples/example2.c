72c4ca255174cb03709c8df7da7bc74e1b48852d
drifter1 <drifterkosmas@gmail.com>
2026-04-06 15:46:38 +0300

    update example2.c

--- examples/simple_c_examples/example2.c
+++ examples/simple_c_examples/example2.c
@@ -1,17 +1,11 @@
-// functions
-double operation(double value, int i) { /* function declaration */
-    // declarations
+double operation(double value, int i) {
     double res;
-    // statements
     res = value * i + i;
     return res;
 }
-// main function
 int main() {
-    // declarations
     int i;
     double val = 2.5, res;
-    // statements
     for (i = 0; i < 10; i++) {
         res = operation(val, i);
         val = res;
