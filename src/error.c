9266816b14030666ef3afb17b88eb8278a071791
drifter1 <drifterkosmas@gmail.com>
2026-04-14 16:14:36 +0300

    print line lexical error occurs alongside the message

--- src/error.c
+++ src/error.c
@@ -1,7 +1,6 @@
 #include "../include/compiler.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
+
+extern const char *filename; // defined in compiler.c
 
 void internal_error(const char *error) {
     perror(error);
@@ -12,8 +11,27 @@ void lexical_analysis_error(const char *error, ...) {
     va_list args;
     va_start(args, error);
 
-    fprintf(stderr, RED "Lexical error at line %d. " CRESET, yylineno);
+    fprintf(stderr, BWHT "%s:%d: " BRED "error: " CRESET, filename, yylineno);
     vfprintf(stderr, error, args);
+
+    char line[256];
+    int count = 0;
+    FILE *fp;
+
+    if (!(fp = fopen(filename, "r")))
+        internal_error(filename);
+
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        count++;
+        if (count == yylineno) {
+            fprintf(stderr, "\n    %4d | %s", yylineno, line);
+        }
+    }
+
+    if (fclose(fp))
+        internal_error(filename);
+
+    fprintf(stderr, "         |");
     fprintf(stderr, " \n");
 
     va_end(args);
