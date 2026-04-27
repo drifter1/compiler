8eca181ca9d6a2984af0bbe554516b82f92db24e
drifter1 <drifterkosmas@gmail.com>
2026-04-27 19:08:41 +0300

    added 2 more examples to examples/simple_c_examples (related to #9)

---
+++ examples/simple_c_examples/constant_test.c
@@ -0,0 +1,11 @@
+int main() {
+    int n1 = 0, n2 = 10, n3 = 191;
+    float x;
+    char c = 'a', esc = '\n';
+    x = .0;
+    x = 0.0;
+    x = .52;
+    x = 2.;
+    x = 3.14;
+    return 0;
+}
---
+++ examples/simple_c_examples/identifier_test.c
@@ -0,0 +1,10 @@
+void foo(){
+    return;
+}
+int main(){
+    int a;
+    float x1;
+    double y55;
+    char e1a2b;
+    return 0;
+}
